8cb3256ffc7ee31167166026a1dc35991212ef01
novalsofyan <59356604+novalsofyan@users.noreply.github.com>
2025-06-12 02:22:49 +0700

    belajar function js

---
+++ webdev/javascript/7/script.js
@@ -0,0 +1,8 @@
+function jumlahVolumeDuaKubus(a, b) {
+  return parseInt(a ** 3 + b ** 3);
+}
+
+console.log("jumlah volume kubus A = 4, dan B = 5 adalah " + jumlahVolumeDuaKubus(4, 5));
+console.log("jumlah volume kubus A = 10, dan B = 23 adalah " + jumlahVolumeDuaKubus(10, 23));
+console.log("jumlah volume kubus A = 11, dan B = 55 adalah " + jumlahVolumeDuaKubus(11, 55));
+console.log("jumlah volume kubus A = 32, dan B = 72 adalah " + jumlahVolumeDuaKubus(32, 72));
