8c1b13ddc5ad07f5cd03c35fb541bb7c11fab439
novalsofyan <59356604+novalsofyan@users.noreply.github.com>
2025-06-15 03:10:30 +0700

    [latihan web - 6] refactoring plus fitur Dark/Light Mode

--- webdev/latihan-web/web-6-navbar-withJS/css/main.js
+++ webdev/latihan-web/web-6-navbar-withJS/css/main.js
@@ -3,6 +3,7 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".navMenu");
 const navLinks = document.querySelectorAll(".navMenuItem");
+const DLMode = document.querySelector(".DLMode");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
@@ -16,3 +17,25 @@ navLinks.forEach((link) => {
     navMenu.classList.remove("active");
   });
 });
+
+// Pengaturan Night atau Light Mode
+
+DLMode.addEventListener("click", () => {
+  document.body.classList.toggle("lightMode");
+  const isLight = document.body.classList.contains("lightMode");
+
+  // Ganti teks tombol
+  DLMode.textContent = isLight ? "⏾" : "☀";
+
+  // Simpan status  ke localStorage
+  localStorage.setItem("theme", isLight ? "light" : "dark");
+});
+
+// Saat halaman dibuka, cek localStorage untuk memuat tema
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme === "light") {
+  document.body.classList.add("lightMode");
+  DLMode.textContent = "⏾";
+} else {
+  DLMode.textContent = "☀";
+}
